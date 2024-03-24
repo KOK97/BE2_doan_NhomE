b143777895acb851c581649e74ee7275fea9ebfb
KOK97 <phatsucvat0907@gmail.com>
2024-03-24 15:20:03 +0700

    update

--- doan_nhomE/resources/views/wishlist.blade.php
+++ doan_nhomE/resources/views/wishlist.blade.php
@@ -46,13 +46,14 @@
         </div>
         <!--====== End - Section Intro ======-->
 
+        @if (auth()->check())
 
         <!--====== Section Content ======-->
         <div class="section__content">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12">
-
+                        @if (count($products) != 0)
                         <!--====== Wishlist Product ======-->
                         <div class="w-r u-s-m-b-30">
                             <div class="w-r__container">
@@ -87,78 +88,6 @@
                             </div>
                         </div>
                         <!--====== End - Wishlist Product ======-->
-
-
-                        <!--====== Wishlist Product ======-->
-                        <div class="w-r u-s-m-b-30">
-                            <div class="w-r__container">
-                                <div class="w-r__wrap-1">
-                                    <div class="w-r__img-wrap">
-
-                                        <img class="u-img-fluid" src="images/product/women/product8.jpg" alt="">
-                                    </div>
-                                    <div class="w-r__info">
-
-                                        <span class="w-r__name">
-
-                                            <a href="product-detail.html">New Dress D Nice Elegant</a></span>
-
-                                        <span class="w-r__category">
-
-                                            <a href="shop-side-version-2.html">Women Clothing</a></span>
-
-                                        <span class="w-r__price">$125.00
-
-                                            <span class="w-r__discount">$160.00</span></span>
-                                    </div>
-                                </div>
-                                <div class="w-r__wrap-2">
-
-                                    <a class="w-r__link btn--e-brand-b-2" data-modal="modal" data-modal-id="#add-to-cart">ADD TO CART</a>
-
-                                    <a class="w-r__link btn--e-transparent-platinum-b-2" href="product-detail.html">VIEW</a>
-
-                                    <a class="w-r__link btn--e-transparent-platinum-b-2" href="#">REMOVE</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--====== End - Wishlist Product ======-->
-
-
-                        <!--====== Wishlist Product ======-->
-                        <div class="w-r u-s-m-b-30">
-                            <div class="w-r__container">
-                                <div class="w-r__wrap-1">
-                                    <div class="w-r__img-wrap">
-
-                                        <img class="u-img-fluid" src="images/product/men/product8.jpg" alt="">
-                                    </div>
-                                    <div class="w-r__info">
-
-                                        <span class="w-r__name">
-
-                                            <a href="product-detail.html">New Fashion D Nice Elegant</a></span>
-
-                                        <span class="w-r__category">
-
-                                            <a href="shop-side-version-2.html">Men Clothing</a></span>
-
-                                        <span class="w-r__price">$125.00
-
-                                            <span class="w-r__discount">$160.00</span></span>
-                                    </div>
-                                </div>
-                                <div class="w-r__wrap-2">
-
-                                    <a class="w-r__link btn--e-brand-b-2" data-modal="modal" data-modal-id="#add-to-cart">ADD TO CART</a>
-
-                                    <a class="w-r__link btn--e-transparent-platinum-b-2" href="product-detail.html">VIEW</a>
-
-                                    <a class="w-r__link btn--e-transparent-platinum-b-2" href="#">REMOVE</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--====== End - Wishlist Product ======-->
                     </div>
                     <div class="col-lg-12">
                         <div class="route-box">
@@ -176,10 +105,37 @@
                             </div>
                         </div>
                     </div>
+                    @else
+                    <div class="section__intro u-s-m-b-60">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="section__text-wrap">
+                                        <h1 class="section__heading u-c-secondary">Danh sách trống</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
         <!--====== End - Section Content ======-->
+
+        @else
+        <div class="section__intro u-s-m-b-60">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section__text-wrap">
+                            <h1 class="section__heading u-c-secondary">ĐĂNG NHẬP ĐỂ XEM DANH SÁCH YÊU THÍCH</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
     </div>
     <!--====== End - Section 2 ======-->
 </div>
