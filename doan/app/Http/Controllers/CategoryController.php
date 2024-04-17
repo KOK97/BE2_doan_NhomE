9dd63d15802962b10e29325c95f0ebf9981a76a9
KOK97 <phatsucvat0907@gmail.com>
2024-04-17 13:47:35 +0700

    update

--- doan/app/Http/Controllers/CategoryController.php
+++ doan/app/Http/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::orderBy('created_at', 'desc')->get();
-        return view('', compact('categories'));
+        return view('layout', compact('categories'));
     }
     
 }
