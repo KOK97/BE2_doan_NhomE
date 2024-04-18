9e680035351df10f102ba1447063ad064d072bfc
HungTrumCode <21211tt1604@mail.tdc.edu.vn>
2024-04-18 23:48:29 +0700

    feat: user management

--- doan/routes/web.php
+++ doan/routes/web.php
@@ -1,17 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 ### ADMIN ###
-    Route::GET('/admin', function () {
+Route::GET('/admin', function () {
         return view('admin.index');
-    })->name('admin.index');
+})->name('admin.index');
     //User
     //Product
     //Category
     //Author
     //...
+
+Route::prefix('/admin')->group(function () {
+    Route::GET('/', function () {
+        return view('admin.index');
+    })->name('admin.index');
+    ### USER ###
+    Route::GET('/user', [UserController::class, 'index'])->name('user.index');
+    Route::GET('/create-user', [UserController::class, 'create'])->name('user.create');
+    Route::POST('/store-user', [UserController::class,'store'])->name('user.store');
+    Route::GET('/edit-user/{user_id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::POST('/update-user/{user_id}', [UserController::class, 'update'])->name('user.update');
+    Route::POST('/delete-user/{user_id}', [UserController::class, 'destroy'])->name('user.destroy');
+});
